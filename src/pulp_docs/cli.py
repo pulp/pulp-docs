bf89693c3cfe1d5944f42a66be8083ab481fe748
Matthias Dellweg <mdellweg@redhat.com>
2025-04-25 12:03:34 +0200

    Add flag to disable blog building

--- src/pulp_docs/cli.py
+++ src/pulp_docs/cli.py
@@ -1,13 +1,26 @@
 import click
 
 from mkdocs.__main__ import cli as mkdocs_cli
-from pulp_docs.context import ctx_draft
+from pulp_docs.context import ctx_blog, ctx_draft
+
+
+def blog_callback(ctx: click.Context, param: click.Parameter, value: bool) -> bool:
+    ctx_blog.set(value)
+    return value
 
 
 def draft_callback(ctx: click.Context, param: click.Parameter, value: bool) -> bool:
     ctx_draft.set(value)
     return value
 
+
+blog_option = click.option(
+    "--blog/--no-blog",
+    default=True,
+    expose_value=False,
+    callback=blog_callback,
+    help="Don't fail if repositories are missing.",
+)
 
 draft_option = click.option(
     "--draft/--no-draft",
@@ -22,3 +35,5 @@ build_command = main.commands.get("build")
 serve_command = main.commands.get("serve")
 draft_option(build_command)
 draft_option(serve_command)
+blog_option(build_command)
+blog_option(serve_command)
--- src/pulp_docs/context.py
+++ src/pulp_docs/context.py
@@ -1,4 +1,5 @@
 from contextvars import ContextVar
 
 
+ctx_blog = ContextVar("ctx_blog", default=True)
 ctx_draft = ContextVar("ctx_draft", default=False)
