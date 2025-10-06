bd47ba766d4a2786a9ab36a60e4c6fe5adf3e5aa
Pedro Brochado <pedropsb95@gmail.com>
2025-10-06 16:36:44 -0300

    Fix build script for docs publishing

--- .github/workflows/scripts/build_all_docs.sh
+++ .github/workflows/scripts/build_all_docs.sh
@@ -7,5 +7,5 @@ set -euv
 FETCHDIR="/tmp/fetchdir"
 pip install .
 pulp-docs fetch --dest "$FETCHDIR"
-pulp-docs build --path "$FETCHDIR"
+pulp-docs build --path "pulp-docs@..:$FETCHDIR"
 tar cvf pulpproject.org.tar site
