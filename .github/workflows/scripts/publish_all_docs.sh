3fc138fbd17188b5bc8d0783deb2ba57d1688b83
Pedro Brochado <pedropsb95@gmail.com>
2026-05-11 11:54:03 -0300

    Refactor publish CI script for cleaner data flow

--- .github/workflows/scripts/publish_all_docs.sh
+++ .github/workflows/scripts/publish_all_docs.sh
@@ -5,6 +5,9 @@ set -euv
 # make sure this script runs at the repo root
 cd "$(dirname "$(realpath -e "$0")")/../../.."
 
+SITE_DIR="$1"
+
+# setup credentials
 mkdir ~/.ssh
 touch ~/.ssh/pulp-infra
 chmod 600 ~/.ssh/pulp-infra
@@ -13,15 +16,14 @@ echo "$PULP_DOCS_KEY" > ~/.ssh/pulp-infra
 echo "docs.pulpproject.org,8.43.85.236 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBGXG+8vjSQvnAkq33i0XWgpSrbco3rRqNZr0SfVeiqFI7RN/VznwXMioDDhc+hQtgVhd6TYBOrV07IMcKj+FAzg=" >> ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 
-# start the ssh agent
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/pulp-infra
 
-mkdir -p ../pulpproject.org
-tar -xvf pulpproject.org.tar --directory ../pulpproject.org
-pushd ../pulpproject.org
-
 # publish to pulpproject.org
-rsync -avzh --delete site/ doc_builder_staging_pulp_core@docs.pulpproject.org:/var/www/docs.pulpproject.org/staging_pulp_core/
-
-popd
+RSYNC_HOST="doc_builder_staging_pulp_core@docs.pulpproject.org"
+RSYNC_PATH="/var/www/docs.pulpproject.org/staging_pulp_core/"
+rsync \
+    -avzh \
+    --delete \
+    "${SITE_DIR}/" \
+    "${RSYNC_HOST}:${RSYNC_PATH}"
